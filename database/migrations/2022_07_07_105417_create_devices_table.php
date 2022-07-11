4877cd35f38c2a458fa27d748a82c210f1e4d1bd
Shegun Babs <shegun.babs@gmail.com>
2022-07-11 16:59:40 +0100

    update device migration

--- database/migrations/2022_07_07_105417_create_devices_table.php
+++ database/migrations/2022_07_07_105417_create_devices_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('serial')->nullable();
+            $table->string('pin')->nullable();
             $table->timestamps();
         });
     }
