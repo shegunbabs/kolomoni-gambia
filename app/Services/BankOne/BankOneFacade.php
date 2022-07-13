c33ca9ee09d6ab4388e3c3fe1abfc286f3ecf3f7
Shegun Babs <shegun.babs@gmail.com>
2022-07-13 05:48:46 +0100

    added static methods for type hinting

--- app/Services/BankOne/BankOneFacade.php
+++ app/Services/BankOne/BankOneFacade.php
@@ -6,6 +6,17 @@ namespace App\Services\BankOne;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class BankOneFacade
+ * @package App\Services\BankOne
+ * @method static getAccountByAccountNumber(string $accountNumber)
+ * @method static getAccountByCustomerId(string $customerID)
+ * @method static balanceEnquiry(string $customerID)
+ * @method static doNameEnquiry(string $accountNumber)
+ * @method static getTransactions(string $accountNumber, string $fromDate, $toDate = null, int $numberOfItems = 200)
+ * @method static createAccount(array $payload)
+ */
+
 class BankOneFacade extends Facade
 {
 
