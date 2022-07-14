7c7c249ddfca6e3bfd41d8cfb11c425c309d7d79
Shegun Babs <shegun.babs@gmail.com>
2022-07-14 17:55:30 +0100

    Name enquiry api

---
+++ app/Http/Controllers/API/NameEnquiryController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Controllers\API;
+
+
+use App\Helpers\ApiResponse;
+use App\Models\Account;
+use App\Services\BankOne\BankOneFacade;
+use Illuminate\Http\Request;
+
+class NameEnquiryController
+{
+
+    public function __invoke(Request $request) {
+        $request->validate([
+            'account_number' => 'required',
+        ]);
+        $account = Account::query()->where('bankone_account_number', $request->account_number)->first();
+
+        $enquiry = BankOneFacade::doNameEnquiry($account->account_number);
+
+        $out['name'] = $enquiry['Name'];
+        $out['account_number'] = $request->account_number;
+        return ApiResponse::success('Account name retrieved', $out);
+    }
+}
