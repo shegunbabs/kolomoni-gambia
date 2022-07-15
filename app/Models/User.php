b0c42a33212d0288b805e3664e1c84b3b09b0297
Shegun Babs <shegun.babs@gmail.com>
2022-07-15 13:51:38 +0100

    add otp_secret to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'firstname', 'lastname', 'email', 'phone', 'status',
-        'password', 'tin', 'dob', 'email_verified_at'
+        'password', 'tin', 'dob', 'email_verified_at', 'otp_secret',
     ];
 
     /**
