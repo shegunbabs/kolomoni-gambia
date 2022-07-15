f718e110d81642adf53e8a81daad4da52968d615
Shegun Babs <shegun.babs@gmail.com>
2022-07-15 16:09:11 +0100

    return comma balances

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -18,6 +19,31 @@ class Account extends Model
         'transaction_tracking_ref', 'customer_id', 'account_number',
         'account_officer_code', 'user_id', 'created_at', 'updated_at'
     ];
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// ACCESSORS & MUTATORS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    protected function availableBalance(): Attribute {
+        return Attribute::make(
+            get: fn ($value) => $value/100.00
+        );
+    }
+
+    protected function ledgerBalance(): Attribute {
+        return Attribute::make(
+            get: fn ($value) => $value/100.00
+        );
+    }
+
+
+    protected function withdrawableBalance(): Attribute {
+        return Attribute::make(
+            get: fn ($value) => $value/100.00
+        );
+    }
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// RELATIONSHIPS
